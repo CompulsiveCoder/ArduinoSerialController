018e7b7e4d76a237d0fc27e8a889557425e94ee6
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-20 22:31:46 +0000

    Added install git hooks script and fixed clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #pio run --target clean
 
-SOURCE_FILE="src/LightPRSensorCalibratedLight/LightPRSensorCalibratedLight.ino"
+SOURCE_FILE="src/ArduinoSerialController/ArduinoSerialController.ino"
 
 sed -i "s/#define VERSION .*/#define VERSION \"1-0-0-0\"/" $SOURCE_FILE
